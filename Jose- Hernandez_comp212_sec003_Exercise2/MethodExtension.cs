2603db5fac55aee5aa001c20cbf621f6fad6b438
josehern <76707012+josehern@users.noreply.github.com>
2021-01-03 22:15:15 -0500

    Add files via upload

---
+++ Jose- Hernandez_comp212_sec003_Exercise2/MethodExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jose__Hernandez_comp212_sec003_Exercise2
+{
+
+  public static class MethodExtension
+    {   
+        
+        //method to count elements of Builder
+        public static int CountWords(this StringBuilder builder)
+        {
+            var Count = builder.Length ;
+         
+            Console.WriteLine("your StringBuilder contians {0} elements ", Count);
+
+            return Count;
+        }
+
+    }
+}
---
+++ Jose- Hernandez_comp212_sec003_Exercise2/MethodExtensionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Jose__Hernandez_comp212_sec003_Exercise2
+{
+    class MethodExtensionTest
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 16; i++)
+            {
+                // append to StringBuilder.
+                builder.Append(i).Append(" ");
+            }
+            Console.WriteLine("{0}", builder);
+
+          
+          MethodExtension.CountWords(builder);
+            
+        }
+    }
+}
